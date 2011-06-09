1edc15f918adebe10583cc1337c32d6029ef3d20
Woojong Koh <wjngkoh@gmail.com>
2011-06-09 14:03:53 +0000

    run_all.bat 파일 추가

---
+++ hw5/run_all.bat
@@ -0,0 +1,3 @@
+hw5.exe 0 6 4 1024 0 # 순서대로 (scene 번호, supersampling 개수, shadow ray 개수, 가로 해상도, BSP on/off)
+hw5.exe 1 6 4 1024 0 # 순서대로 (scene 번호, supersampling 개수, shadow ray 개수, 가로 해상도, BSP on/off)
+hw5.exe 2 6 4 1024 0 # 순서대로 (scene 번호, supersampling 개수, shadow ray 개수, 가로 해상도, BSP on/off)
